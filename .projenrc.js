a1aa459dbbcab986fd098d61c2419ba252760ed5
Neil Kuan <guan840912@gmail.com>
2020-11-21 15:57:13 +0800

    bump projen version

--- .projenrc.js
+++ .projenrc.js
@@ -7,7 +7,7 @@ const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 const project = new AwsCdkConstructLibrary({
   authorAddress: 'guan840912@gmail.com',
   authorName: 'Neil Kuan',
-  cdkVersion: '1.72.0',
+  cdkVersion: '1.74.0',
   description: PROJECT_DESCRIPTION,
   name: PROJECT_NAME,
   repository: 'https://github.com/guan840912/cdk-events-notify.git',
