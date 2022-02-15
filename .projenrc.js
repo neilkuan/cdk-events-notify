16c3ddab4591a36ea74c41beb051687805a07dda
neilkuan <guan840912@gmail.com>
2022-02-15 09:19:37 +0000

    chore(deps) update deps

--- .projenrc.js
+++ .projenrc.js
@@ -9,7 +9,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: PROJECT_NAME,
   repository: 'https://github.com/neilkuan/cdk-events-notify.git',
   keywords: ['aws', 'cdk', 'events', 'notify'],
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.12.0',
   /**
    * we default release the main branch(cdkv2) with major version 2.
    */
