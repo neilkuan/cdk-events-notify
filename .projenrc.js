a39e6cebf4c614951054e3fe607c1ecd2336379e
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2022-09-04 11:25:52 +0800

    chore(deps): upgrade dependencies (#719)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/neilkuan/cdk-events-notify/actions/runs/2986242877
    
    ------
    
    *Automatically created by projen via the "upgrade-cdkv1" workflow*

--- .projenrc.js
+++ .projenrc.js
@@ -57,10 +57,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   peerDeps: [
     '@aws-cdk/core@^1.134.0',
   ],
-  typescriptVersion: '4.6.2',
+  typescriptVersion: '3.9.10',
 });
+
 project.package.addField('resolutions', {
   'trim-newlines': '3.0.1',
+  'got': '12.3.0',
 });
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage', '.env', '.DS_Store'];
 project.gitignore.exclude(...common_exclude);
